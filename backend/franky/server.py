03e090a3cc68fcfc4395a1e918f9cfb68fa99d56
Andrei Tcibin <Andrei_Tcibin@epam.com>
2019-04-10 23:51:32 +0300

    feat: Add basic github service implementation

--- backend/franky/server.py
+++ backend/franky/server.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from service import GitHub
+from utils import json_endpoint
 
 app = Flask(__name__)
 
 
-@app.route('/ping')
+@app.route('/ping', methods=['GET'])
 def ping():
     return 'From franky with ping!'
+
+
+@app.route('/api/github/<username>', methods=['GET'])
+@json_endpoint
+def github(username):
+    return GitHub().user(username)
 
 
 if __name__ == '__main__':
