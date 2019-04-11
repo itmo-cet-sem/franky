670cffa23b85b52dd4a07e3d24a783b83a099ec2
dyadyaJora <ja_massa97@mail.ru>
2019-04-11 14:13:54 +0300

    TEST containers

---
+++ backend/franky/server.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/ping')
+def ping():
+    return 'From franky with ping!'
+
+
+docker_host=os.getenv('DOCKER_NETWORK_HOST') or '0.0.0.0'
+
+if __name__ == '__main__':
+    app.run(host=docker_host)
