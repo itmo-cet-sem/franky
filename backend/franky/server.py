90e1839c8f4116f7db66dd169169cc4172b731ff
Andrei Tcibin <Andrei_Tcibin@epam.com>
2019-04-18 23:27:48 +0300

    feat: Add github projects endpoint

--- backend/franky/server.py
+++ backend/franky/server.py
@@ -1,23 +1,31 @@
 import os
+from typing import List
+
 from flask import Flask
+
+from model import UserData
+from model.project import ProjectData
 from service import GitHub
-from utils import json_endpoint
+from utils import convert_to_json
 
 app = Flask(__name__)
 
 
 @app.route('/ping', methods=['GET'])
-def ping():
+def ping() -> str:
     return 'From franky with ping!'
 
 
 @app.route('/api/github/<username>', methods=['GET'])
-@json_endpoint
-def github(username):
-    return GitHub().user(username)
+def github_user(username) -> UserData:
+    return convert_to_json(GitHub().user(username))
 
 
-docker_host=os.getenv('DOCKER_NETWORK_HOST') or '0.0.0.0'
+@app.route('/api/github/<username>/projects', methods=['GET'])
+def github_projects(username) -> List[ProjectData]:
+    return convert_to_json(GitHub().projects(username))
+
 
 if __name__ == '__main__':
+    docker_host = os.getenv('DOCKER_NETWORK_HOST', '0.0.0.0')
     app.run(host=docker_host)
