a322c76177ef4437976645d85ab74542f1e03764
dyadyaJora <ja_massa97@mail.ru>
2019-04-20 15:04:47 +0300

    lint fi

--- frontend/src/containers/Observe/Observe.js
+++ frontend/src/containers/Observe/Observe.js
@@ -4,18 +4,7 @@ import SearchBlock from '../SearchBlock/SearchBlock';
 import ActivityInfo from '../ActivityInfo/ActivityInfo';
 
 class Observe extends Component {
-	componentDidMount() {
-  	// const { dispatch } = this.props;
-  	// console.log(dispatch({ type: 'TEST' }));
-	}
-
   render() {
-  	console.log(this.props);
-  	// let x = fetch('/api/github/dyadyajora')
-  	// 	.then(res => res.json())
-  	// 	.then(json => console.log(json))
-
-  	// console.log(x);
     return (
       <div>
         <h3>Try iy now!</h3>
@@ -27,5 +16,5 @@ class Observe extends Component {
 }
 
 export default connect(
-	state => ({ info: state.observableInfo, test: state.test })
+  state => ({ info: state.observableInfo })
 )(Observe);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import testReducer from './testReducer';
 import observableReducer from './observableReducer';
 
 let reducers = combineReducers({
-  test: testReducer,
   observableInfo: observableReducer
 });
 
